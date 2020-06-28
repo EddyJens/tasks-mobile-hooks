8d57278a78c3f2a551700e0b30ca06efbf3b88f9
Eddy Jens Rivero Zavala <eddy@carefy.com.br>
2020-06-28 11:09:14 -0300

    the first commit

---
+++ src/components/Task.js
@@ -0,0 +1,142 @@
+import React from 'react'
+import { View, Text, StyleSheet, TouchableWithoutFeedback, TouchableOpacity } from 'react-native'
+import commonStyles from '../commonStyles'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import moment from 'moment'
+import 'moment/locale/pt-br'
+import Swipeable from 'react-native-gesture-handler/Swipeable'
+
+export default props => {
+
+  const doneOrNotStyle = props.doneAt !== null ?
+    { textDecorationLine: 'line-through' } : {}
+
+  const date = props.doneAt ? props.doneAt : props.estimateAt
+  const formattedDate = moment(date).locale('pt-br')
+    .format('ddd, D [de] MMMM')
+
+  const getRightContent = () => {
+    return (
+      <TouchableOpacity style={styles.right}
+        onPress={() => props.onDelete && props.onDelete(props.id)}>
+        <Icon name="trash" size={30} color="#fff"/>
+      </TouchableOpacity>
+    )
+  }
+
+  const getLeftContent = () => {
+    return (
+      <View style={styles.left}>
+        <Icon name="trash" size={20} color="#fff"/>
+        <Text style={styles.excludeText}>Excluir</Text>
+      </View>
+    )
+  }
+
+  return (
+    <Swipeable
+      renderRightActions={getRightContent}
+      renderLeftActions={getLeftContent}
+      onSwipeableLeftOpen={() => props.onDelete && props.onDelete(props.id)}>
+      <View style={styles.container}>
+        <TouchableWithoutFeedback
+          onPress={() => props.toggleTask(props.id)}>
+          <View style={styles.checkContainer}>
+            {getCheckView(props.doneAt)}
+          </View>
+        </TouchableWithoutFeedback>
+        <View>
+          <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
+          <Text style={styles.date}>{formattedDate}</Text>
+        </View>
+      </View>
+    </Swipeable>
+  )
+}
+
+function getCheckView(doneAt) {
+  if (doneAt !== null) {
+    return (
+      <View style={styles.done}>
+        <Text>
+          <Icon name='check' 
+            size={20} 
+            color='#FFF' 
+            style={styles.excludeIcon}></Icon>
+        </Text>
+      </View>
+    )
+  } else {
+    return (
+      <View style={styles.pending}>
+        <Text>
+
+        </Text>
+      </View>
+    )
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    borderColor: '#AAA',
+    borderBottomWidth: 1,
+    alignItems: 'center',
+    paddingVertical: 10,
+    backgroundColor: 'white'
+  },
+  checkContainer: {
+    width: '20%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  pending: {
+    height: 25,
+    width: 25,
+    borderRadius: 13,
+    borderWidth: 1,
+    borderColor: '#555'
+  },
+  done: {
+    height: 25,
+    width: 25,
+    borderRadius: 13,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  desc: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.mainText,
+    fontSize: 15
+  },
+  date: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.subText,
+    fontSize: 12
+  },
+  right: {
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20
+  },
+  left: {
+    flex: 1,
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  excludeText: {
+    fontFamily: commonStyles.fontFamily,
+    color: '#FFF',
+    fontSize: 20,
+    margin: 10
+  },
+  excludeIcon: {
+    marginLeft: 10
+  }
+})
